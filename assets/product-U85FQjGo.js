037057d5d79f382d608df67f09677916647ca4ea
Panchenko Denis <Pandenisko@gmail.com>
2026-02-23 23:15:36 +0200

    Updates

--- assets/product-U85FQjGo.js
+++ assets/product-U85FQjGo.js
@@ -1,4 +1,4 @@
-import{H as k,g as C,a as S,b as P,u as w,T as l}from"./app-C0EzfPww.js";import{g as q,b as I,c as T,r as L}from"./url-7D2TFkBX.js";const F=`<section class="product">\r
+import{H as k,g as C,a as S,b as P,u as w,T as l}from"./app-CpNPevsP.js";import{g as q,b as I,c as T,r as L}from"./url-CqpyPdCF.js";const F=`<section class="product">\r
     <div class="product__wrapper">\r
         <div class="product__image-wrapper {{categoryClass}}">\r
             <img\r
